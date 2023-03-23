d866c9294b347d26dd6fafde2031e411619cb3d9
Dona Saji <dona.saji@e-xposures.com>
2023-03-23 12:13:04 +0530

    Build test

--- src/main/java/com/example/JenkinsDemo/JenkinsDemoApplication.java
+++ src/main/java/com/example/JenkinsDemo/JenkinsDemoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinsDemoApplication {
 
 	public static void main(String[] args) {
-		
 		SpringApplication.run(JenkinsDemoApplication.class, args);
 	}
 
